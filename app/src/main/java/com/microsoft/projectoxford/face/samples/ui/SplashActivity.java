3fc043c56ce1b36d9c2a7df87fb1785c22288d99
anurag kolhe <kolheab15.it@coep.ac.in>
2018-04-12 02:27:38 +0530

    New Camera permissions added

--- app/src/main/java/com/microsoft/projectoxford/face/samples/ui/SplashActivity.java
+++ app/src/main/java/com/microsoft/projectoxford/face/samples/ui/SplashActivity.java
@@ -4,21 +4,38 @@ package com.microsoft.projectoxford.face.samples.ui;
  * Created by anurag on 11/4/18.
  */
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.microsoft.projectoxford.face.samples.R;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity  {
     String password;
+    private static final int PERMISSION_REQUEST_CAMERA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //Camera Permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Permission is already available, start camera preview
+        } else {
+            // Permission is missing and must be requested.
+            requestCameraPermission();
+        }
 
         //load the password
         SharedPreferences settings = getSharedPreferences("PREFS", 0);
@@ -43,5 +60,38 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, 2000);
     }
+
+    //Camera Permissions
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        // BEGIN_INCLUDE(onRequestPermissionsResult)
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            // Request for camera permission.
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted. Start camera preview Activity.
+            }
+        }
+        else {
+            Toast.makeText(SplashActivity.this, "Permissions denied", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        // END_INCLUDE(onRequestPermissionsResult)
+    }
+    private void requestCameraPermission() {
+        // Permission has not been granted and must be requested.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // Display a SnackBar with cda button to request the missing permission.
+
+        } else {
+            // Request the permission. The result will be received in onRequestPermissionResult().
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+        }
+    }
+
 }
 
